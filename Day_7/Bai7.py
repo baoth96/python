8f27e461e97dcd2918debd2ee5b97420cd4e569c
baoth96 <thaihoangbao112@gmail.com>
2022-11-18 10:44:16 +0700

    add

--- Day_7/Bai7.py
+++ Day_7/Bai7.py
@@ -18,43 +18,40 @@
 #         self.level = level
 
 #     def intro(self):
-#         return self.name + "Level" + str(self.level)
+#         return self.name + " (Level " + str(self.level) + ")"
 
 # tony = Player("Tony", 12)
 # print(tony.intro())
 
 # ---------------------------------------------------------------------------------
 
+# BÀI TẬP 2:
+# You are making a drawing application, which has a Shape base class.
+# The given code defines a Rectangle class, creates a Rectangle objects and calls
+# its area() and perimeter() methods.
+# Do the following to complete the program:
+# 1. Inherit the Rectangle class from Shape
+# 2. Define the perimeter() method in the Rectangle class, printing the perimeter of the Rectangle.
 
-# class Pet:
-#     def __init__(self, name, favorite_food):
-#         self.name = name
-#         self.favorite_food = favorite_food
+# GIẢI:
 
-#     def eat(self):
-#         print("I'm eating" + self.favorite_food)
+# class Shape:
+#     def __init__(self, dai, rong):
+#         self.dai = dai
+#         self.rong = rong
 
-# class Cat(Pet):  
-#     def climb_tree(self):
-#         print(self.name, " can climb tree")
-
-# mao_mao = Cat("Mao Mao", "fish")
-# mao_mao.eat()
-# mao_mao.climb_tree()
-
-
-class Shape:
-    def __init__(self, dai, rong):
-        self.dai = dai
-        self.rong = rong
-
-    def area(self):
-        return self.dai * self.rong
+#     def area(self):
+#         return self.dai * self.rong
     
-class Rectangle(Shape):
-    def perimeter(self):
-        return (self.dai + self.rong) * 2
+# class Rectangle(Shape):
+#     def perimeter(self):
+#         return (self.dai + self.rong) * 2
 
-dai = Rectangle(5, 6)
-print(dai.perimeter())
-print(dai.area())
+# dai = Rectangle(5, 6)
+# print(dai.perimeter())
+# print(dai.area())
+
+# ---------------------------------------------------------------------------------
+
+# BÀI TẬP VỀ NHÀ:
+
