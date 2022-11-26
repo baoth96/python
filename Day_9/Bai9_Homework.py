423fb4a675e01b33b9448619b93e2bfbdbc287c3
baoth96 <thaihoangbao112@gmail.com>
2022-11-26 10:20:33 +0700

    add

--- Day_9/Bai9_Homework.py
+++ Day_9/Bai9_Homework.py
@@ -36,4 +36,31 @@
 
 # GIẢI:
 
-x = int(input())
+# def palindrome(x):
+#     if x[::-1] == x:
+#         print("True")
+#     else:
+#         print("False")
+
+# x = int(input("x = "))
+# y = str(x)
+# palindrome(y)
+
+# ------------------------------------------------------------------------------------------------
+
+# BÀI 3:
+# You are given a large integer represented as an integer array digits, 
+# where each digits[i] is the ith digit of the integer. 
+# The digits are ordered from most significant to least significant in left-to-right order. 
+# The large integer does not contain any leading 0's.
+# Increment the large integer by one and return the resulting array of digits.
+
+# Example 1:
+# Input: digits = [1,2,3]
+# Output: [1,2,4]
+# Explanation: The array represents the integer 123.
+# Incrementing by one gives 123 + 1 = 124.
+# Thus, the result should be [1,2,4].
+
+# GIẢI:
+
