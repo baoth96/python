ea0617e4437acca056fd9ee0a43c54f229ce585d
baoth96 <thaihoangbao112@gmail.com>
2022-11-28 18:07:27 +0700

    add

--- Day_9/Bai9_Homework.py
+++ Day_9/Bai9_Homework.py
@@ -17,16 +17,17 @@
 # GIẢI:
 
 nums = [1, 7, 3, 6, 5, 6]
-
 sumL = 0
 sumR = sum(nums)
+j = -1
+
 for i in range(len(nums)):
     sumR = sumR - nums[i]
     if sumL == sumR:
-        print(i)
+        j = i
     sumL = sumL + nums[i]
 
-
+print(j)
 
 # ------------------------------------------------------------------------------------------------
 
---
+++ Day_9/tempCodeRunnerFile.py
@@ -0,0 +1,12 @@
+nums = [1, 7, 3, 6, 5, 6]
+# sumL = 0
+# sumR = sum(nums)
+# j = -1
+
+# for i in range(len(nums)):
+#     sumR = sumR - nums[i]
+#     if sumL == sumR:
+#         j = i
+#     sumL = sumL + nums[i]
+
+# print(j)
